627252032528af2eb48a3e4453488ea64d9891b6
zyl-hub <54789212+zyl-hub@users.noreply.github.com>
2020-01-10 21:48:11 +0800

    Create mdHead-to-Gitpage-Toc.cpp

---
+++ mdHead-to-Gitpage-Toc.cpp
@@ -0,0 +1,58 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream markdown;
+    //filename of your md
+    markdown.open("/home/yunlang/Desktop/C++复习.md");
+    ofstream toc;
+    //filename of your toc
+    toc.open("/home/yunlang/Desktop/toc.txt");
+    char word='\0';
+    int count;
+    count=0;
+    int i;
+    i=0;
+    bool flag;
+    flag=0;
+    char tocstring[100]={0};
+     while(markdown.get(word)){
+       if(word=='#'){
+         count++;
+         continue;
+       }
+       if(count&&!flag){
+         if(word==' ') flag=1;
+         if(word!='#'&&word!=' ') count=0;
+         continue;
+       }
+       if(flag){
+         tocstring[i]=word;
+         i++;
+       }
+       if(flag&&(word=='\n')){
+         for(int k=0;k<2*(count-1);k++){
+           toc<<' ';
+         }
+         if(count%2!=0) toc<<"- ";
+         if(count%2==0) toc<<"* ";
+         toc<<'[';
+         tocstring[i-1]='\0';
+         toc<<tocstring;
+         toc<<"](#";
+         toc<<tocstring;
+         toc<<')';
+         toc<<'\n';
+         flag=0;
+         count=0;
+         for(i=0;i<100;i++){
+           tocstring[i]='\0';
+         }
+         i=0;
+     }
+       if(word=='\n') count=0;
+  }
+  return 0;
+}
